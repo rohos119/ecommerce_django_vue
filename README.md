299e2eeca4017e9ca57784ed4b1168ec6f7bd2d9
Beom <rohos119@gmail.com>
2022-02-07 21:54:21 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Vue_project
+To Be FullStack Developer
+
+1.Setup
+ 1) Node.js DownLoad
+ 2) cmd > npm --version 
+    npm install yarn
+ 3) VsCode setting
+    - Koeran language pack
+    - vetur
+    - vue vscod snippets
+    - Live server
+    - vscode-icons
+    - auto rename tag
+    - auto close tag
+ 4) vue cli install
+    - npm install -g @vue/cli
+    - cmd > vue create <project_name>
+      Chose what you want to setup
+      
+  
---
+++ ecommerce_vue/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="wrapper">
+    <nav class="navbar">
+      <div class="navbar-brand">
+        <router-link to="/" class="navbar-item"><strong>Fashion Mall</strong></router-link>
+        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu" @click="showMobileMenu = !showMobileMenu" >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+      <div class="navbar-menu" id="navbar-menu" v-bind:class="{'is-active':showMobileMenu}">
+        <div class="navbar-start">
+          <router-link to="/Man" class="navbar-item">Man</router-link>
+          <router-link to="/Women" class="navbar-item">Women</router-link> 
+        </div>
+        <div class="navbar-end">
+          <div class="navbar-item">
+            <div class="buttons">
+              <router-link to="/log-in" class="button is-light">Login</router-link>
+              <router-link to="/cart" class="button is-success">
+                <span class="icon"><i class="fas fa-shopping-cart"></i></span><span>Cart</span>
+              </router-link>
+            </div>
+          </div>
+        </div>     
+      </div>
+      
+    </nav>
+    <section class="section">
+      <router-view/>
+    </section>
+    <footer class="footer">
+      <p>Copyright (c) 2022</p>
+    </footer>
+  </div>
+</template>
+<script>
+  export default{
+    data(){
+      return{
+        //hambuger menu display trigger 
+        showMobileMenu: false,
+        }
+    }
+  }
+</script>
+<style lang="scss">
+@import '../node_modules/bulma';
+
+
+</style>
